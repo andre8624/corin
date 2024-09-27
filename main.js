5b19245d0cbd7af1fee19f01276602b75595d6f0
andre8624 <161709690+andre8624@users.noreply.github.com>
2024-09-27 11:37:05 -0300

    .

--- main.js
+++ main.js
@@ -4,11 +4,14 @@
     return num1+num2;
  }
  console.log(soma(6,4));
+ console.log(soma(10,4));
+ console.log(soma(5,29));
 
  function multi(num1,num2){
    return num1*num2;
 }
 console.log(multi(6,4));
+
 
 
 
